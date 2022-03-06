7b83cc90a1dbe52682b4754d5686da5d835f9a2d
DjGorillaz <djg2008@gmail.com>
2022-03-07 00:14:33 +0300

    Create ppm image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let image_width = 256;
+    let image_height = 256;
+
+    // Render
+    println!("P3\n{} {}\n255", image_width, image_height);
+
+    for j in (0..image_height).rev() {
+        eprintln!("\rScanlines remaining: {}", j);
+        for i in 0.. image_width {
+            let r = i as f64 / (image_width as f64 - 1_f64);
+            let g = j as f64 / (image_height as f64 - 1_f64);
+            let b = 0.25;
+
+            let ir = 255.999 * r;
+            let ig = 255.99 * g;
+            let ib = 255.99 * b;
+
+            println!("{} {} {}", ir as i32, ig as i32, ib as i32);
+        }
+    }
+
+    eprintln!("Done!");
 }
