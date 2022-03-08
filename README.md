b07ce80d9fdd7507360583072ab5afd89bfd6651
DjGorillaz <djg2008@gmail.com>
2022-03-08 19:21:46 +0300

    Remove Cargo.lock

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # rust-ray-tracer
 
 Implementing [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html)
+
+# Current state
+![](image.png)
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 mod ray;
 mod vec3;
 
+use image::ImageBuffer;
 use ray::Ray;
 use vec3::*;
 
 fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
     let oc = r.origin() - center;
     let a = dot(&r.direction(), &r.direction());
-    let b =  2.0 * dot(&r.direction(), &oc);
+    let b = 2.0 * dot(&r.direction(), &oc);
     let c = dot(&oc, &oc) - radius.powf(2.0);
 
     let discriminant = b.powf(2.0) - 4.0 * a * c;
@@ -22,24 +23,23 @@ fn ray_color(r: &Ray) -> Color {
 
     let unit_direction = unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
-    
+
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
-fn write_color(pixel_color: &Color) {
-    println!(
-        "{} {} {}",
-        (255.999 * pixel_color.x()) as i32,
-        (255.999 * pixel_color.y()) as i32,
-        (255.999 * pixel_color.z()) as i32
-    );
+fn create_pixel(pixel_color: &Color) -> image::Rgb<u8> {
+    image::Rgb([
+        (255.999 * pixel_color.x()) as u8,
+        (255.999 * pixel_color.y()) as u8,
+        (255.999 * pixel_color.z()) as u8,
+    ])
 }
 
 fn main() {
     // Image
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400;
-    let image_height = (image_width as f64 / aspect_ratio) as i32;
+    let image_height = (image_width as f64 / aspect_ratio) as u32;
 
     // Camera
     let viewport_height = 2.0;
@@ -53,22 +53,24 @@ fn main() {
         origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     // Render
-    println!("P3\n{} {}\n255", image_width, image_height);
+    let mut img = ImageBuffer::new(image_width, image_height);
 
-    for j in (0..image_height).rev() {
-        eprintln!("\rScanlines remaining: {}", j);
+    for j in 0..image_height {
         for i in 0..image_width {
             let u = i as f64 / (image_width - 1) as f64;
-            let v = j as f64 / (image_height - 1) as f64;
+            let v = (image_height - j) as f64 / (image_height - 1) as f64;
 
             let r = Ray::new(
                 origin,
                 lower_left_corner + u * horizontal + v * vertical - origin,
             );
             let pixel_color = ray_color(&r);
-            write_color(&pixel_color);
+            let pixel = create_pixel(&pixel_color);
+            img.put_pixel(i, j, pixel);
         }
     }
 
-    eprintln!("Done!");
+    img.save("image.png").expect("failed to save image!");
+
+    println!("Done!");
 }
