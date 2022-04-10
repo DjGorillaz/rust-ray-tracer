df77caabafe080acd4ae743e7977113522815a39
DjGorillaz <djg2008@gmail.com>
2022-04-10 18:36:54 +0300

    Add fuzzy reflection

--- src/main.rs
+++ src/main.rs
@@ -71,18 +71,10 @@ fn main() {
     let samples_per_pixel = 100;
     let max_depth = 50;
 
-    let material_ground = Rc::new(Lambertian {
-        albedo: Color::new(0.8, 0.8, 0.0),
-    });
-    let material_center = Rc::new(Lambertian {
-        albedo: Color::new(0.7, 0.3, 0.3),
-    });
-    let material_left = Rc::new(Metal {
-        albedo: Color::new(0.8, 0.8, 0.8),
-    });
-    let material_right = Rc::new(Metal {
-        albedo: Color::new(0.8, 0.6, 0.2),
-    });
+    let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
+    let material_center = Rc::new(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
+    let material_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
+    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
 
     // World
     let mut world = HittableList { objects: vec![] };
--- src/material.rs
+++ src/material.rs
@@ -9,8 +9,14 @@ pub struct Lambertian {
     pub albedo: Color,
 }
 
+impl Lambertian {
+    pub fn new(albedo: Color) -> Self {
+        Lambertian { albedo }
+    }
+}
+
 impl Material for Lambertian {
-    fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)> {
+    fn scatter(&self, _r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)> {
         // let target = rec.p + rec.normal + Vec3::random_in_unit_sphere(); // lambertian approximation
         // let target = rec.p + rec.normal + Vec3::random_unit_vector(); // true lambertian reflection
         // let target = rec.p + Vec3::random_in_hemishpere(&rec.normal); // alternative diffuse method
@@ -29,9 +35,14 @@ impl Material for Lambertian {
 
 pub struct Metal {
     pub albedo: Color,
+    pub fuzz: f64,
 }
 
 impl Metal {
+    pub fn new(albedo: Color, fuzz: f64) -> Metal {
+        Metal {albedo, fuzz}
+    }
+
     fn reflect(&self, v: &Vec3, n: &Vec3) -> Vec3 {
         *v - 2.0 * dot(v, n) * (*n)
     }
@@ -41,7 +52,7 @@ impl Material for Metal {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)> {
         let reflected = self.reflect(&unit_vector(r_in.direction()), &rec.normal);
 
-        let scattered = Ray::new(rec.p, reflected);
+        let scattered = Ray::new(rec.p, reflected + self.fuzz * Vec3::random_in_unit_sphere());
         let attenuation = self.albedo;
 
         if dot(&scattered.direction(), &rec.normal) > 0.0 {
